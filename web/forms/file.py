0f24d2ddb03b653b10003dc632fcc866edec5f79
战点 <2201609540@qq.com>
2025-02-04 23:23:25 +0800

    v3.3.2

--- web/forms/file.py
+++ web/forms/file.py
@@ -9,23 +9,28 @@ class FileModelForm(BootstrapForm, ModelForm):
         model = models.FileRepository
         fields = ['name', ]
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, request, parent_obj, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.request = request
+        self.parent = parent_obj
 
     def clean_name(self):
         # 获取用户提交的文件夹名称
         name = self.cleaned_data.get('name')
         # 获取当前表单对应的模型实例,从中获取父级目录和所属项目
-        parent_object = self.instance.parent
-        project = self.instance.project
+        parent_object = self.parent
+        project = self.request.bugtracer.project  # 从request中获取project
 
-        # 检查同级目录下是否存在重名文件夹
-        exists = models.FileRepository.objects.filter(
-            name=name,
-            project=project,
-            parent=parent_object,
-            file_type=2
-        ).exists()
+        queryset = models.FileRepository.objects.filter(file_type=2, name=name, project=self.request.bugtracer.project)
+
+        # 然后根据是否有父文件夹来判断：
+        if self.parent:
+            # 如果有父文件夹，检查同一父文件夹下是否存在同名文件夹
+            exists = queryset.filter(parent=self.parent).exists()
+        else:
+            # 如果没有父文件夹（即根目录），检查根目录下是否存在同名文件夹
+            # 字段名__查找类型=值
+            exists = queryset.filter(parent__isnull=True).exists()
 
         if exists:
             raise ValidationError('文件夹名称已存在')
--- web/views/file.py
+++ web/views/file.py
@@ -6,14 +6,15 @@ from django.http import JsonResponse
 
 
 def file(request, project_id):
+    parent_obj = None
+    folder_id = request.GET.get('folder', '')
+    if folder_id and folder_id.isdigit():
+        parent_obj = models.FileRepository.objects.filter(project=request.bugtracer.project, id=folder_id, file_type=2)
     # 添加文件夹
     if request.method == 'POST':
-        # 获取父文件夹ID,默认为0
-        parent_obj = None
-        folder_id = request.POST.get('folder', '')
-        if folder_id and folder_id.isdigit():
-            parent_obj = models.FileRepository.objects.filter(project=request.bugtracer.project, id=folder_id, )
-        form = FileModelForm(data=request.POST)
+        # 获取父文件夹ID,默认为空字符串
+
+        form = FileModelForm(request, parent_obj, data=request.POST)
         if form.is_valid():
             form.instance.project = request.bugtracer.project
             form.instance.file_type = 2
@@ -24,5 +25,11 @@ def file(request, project_id):
         else:
             return JsonResponse({'status': False, 'error': form.errors})
     else:
-        form = FileModelForm()
-        return render(request, 'web/file.html/', {'form': form})
+        # 获取当前目录下的文件列表（所有的文件夹和文件）
+        form = FileModelForm(request, parent_obj, )
+        queryset = models.FileRepository.objects.filter(project=request.bugtracer.project)
+        if parent_obj:
+            file_list = queryset.filter(parent=parent_obj).order_by('-file_type')
+        else:
+            file_list = queryset.filter(parent__isnull=True).order_by('-file_type')
+        return render(request, 'web/file.html/', {'form': form, 'file_list': file_list})
